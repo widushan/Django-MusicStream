07efff0e6993a40bb4875a2d67c71b9a0f027cde
widushan <114930601+widushan@users.noreply.github.com>
2025-05-28 13:50:58 +0530

    Second committ
    Build settings, models, views, urls, index and css files

--- MusicPlayer/MusicPlayer/settings.py
+++ MusicPlayer/MusicPlayer/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,8 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
---
+++ MusicPlayer/MusicPlayerapp/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from .import views
+
+
+app_name = "MusicPlayerapp"
+
+
+urlpatterns=[
+    path('', views.index, name="index")
+]

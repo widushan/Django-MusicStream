24401e965d8a835d42b945f0c126ff893e4dd2c8
widushan <114930601+widushan@users.noreply.github.com>
2025-05-28 16:53:39 +0530

    interface commit
    building music player interface , admin songs collection designing

--- MusicPlayer/MusicPlayerapp/urls.py
+++ MusicPlayer/MusicPlayerapp/urls.py
@@ -1,12 +1,13 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
-from .import views
-
+from . import views 
+from django.conf import settings
+from django.conf.urls.static import static 
 
 app_name = "MusicPlayerapp"
 
-
-urlpatterns=[
-    path('', views.index, name="index")
+urlpatterns = [
+    path('', views.index, name='index'),  # Map root URL to your view
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

cbd49fc42a1fdcdaae4bf65c5b26d3fef71a89c5
widushan <114930601+widushan@users.noreply.github.com>
2025-08-19 00:13:47 +0530

    git commit
    hithub side done, next have to render

--- MusicPlayer/MusicPlayer/settings.py
+++ MusicPlayer/MusicPlayer/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-acq0m$49=y96d3-@c1t^q^-7$63on&e6k&%-+f&0$&ul@w341y"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -81,7 +81,9 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-DATABASES["default"] = dj_database_url.parse("postgresql://music_django_render_user:XsXHoTvZHrrAefPTxgJRLrdAiLf9Axjz@dpg-d2hffdadbo4c73b0esl0-a.oregon-postgres.render.com/music_django_render")
+
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
 # postgresql://music_django_render_user:XsXHoTvZHrrAefPTxgJRLrdAiLf9Axjz@dpg-d2hffdadbo4c73b0esl0-a.oregon-postgres.render.com/music_django_render
 
 
